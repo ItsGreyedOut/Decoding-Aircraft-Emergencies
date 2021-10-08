ffb075968e047b9884387d9d98cc4f71ad4bf612
Jack Cohen <>
2021-10-08 07:08:49 -0400

    Added map and event handling, changed some html/css formatting, created filter table with event handling, fixed the telemetry plot issues, working on the filtering function

--- static/visualizations.js
+++ static/visualizations.js
@@ -1,47 +1,57 @@
 console.log(dataset);
+// TO DO:
+// - Filter Function improve/get it to work
+// - Add markers to the map (airport data?? location of start and end are not always takeoff and landing)
+// - Create summary statistics tables
 
-
-// Default Flight Summary Table
-let one = 'ARG1511_20180101';
+// Flight Summary Table
+let icao = dataset[0][0]['ICAO24'][0];
 
 let flight_info = "";
-for (let f=0; f<dataset[1][0]['Flight ID'].length;f++) {
-    if (dataset[1][0]['Flight ID'][f] = one) {
+for (let f=0; f<dataset[1][0]['Callsign'].length;f++) {
+    if (dataset[1][0]['ICAO24'][f] == icao) {
         for (const [key, value] of Object.entries(dataset[1][0])) { 
-            flight_info += key+" : "+value[f]+"<br>";
+            if (value[f] != 'NaN') {
+                flight_info += "<p><span>"+key+":</span> "+value[f]+"</p>";
+            }
         }
     }
 };
-
 document.getElementById("flight_info").innerHTML = flight_info
 
 
-// Default Aircraft Info Table
+// Aircraft Info Table
 let meta = dataset[2][0];
 let aircraft = "";
 for (const [key, value] of Object.entries(meta)) {
-    aircraft += key+" : "+value+"<br>";
+    if (value != 'NaN') {
+        aircraft += "<p><span>"+key+":</span> "+value+"</p>";
+    }
 }
 document.getElementById("aircraft_info").innerHTML = aircraft
 
+// Flight Title
+let flight_title = ""
+flight_title += "<p>Flight "+dataset[0][0]['Callsign'][0]+"</p>";
+document.getElementById("flight_title").innerHTML = flight_title
 
-// Default Altitude/Speed Plot
+// Altitude/Speed Plot
 labels = [];
 for (let i = 0; i < dataset[0][0]['Squawk'].length; i++) {
     labels.push(`Squawk: ${dataset[0][0]['Squawk'][i]}`);
 };
 
 let trace1 = {
-    x: dataset[0][0]['timestamp'],
-    y: dataset[0][0]['altitude'],
+    x: dataset[0][0]['Timestamp'],
+    y: dataset[0][0]['Altitude'],
     text: labels,
     type: 'line',
     name: "Altitude (ft.)"
 };
 
 let trace2 = {
-    x: dataset[0][0]['timestamp'],
-    y: dataset[0][0]['groundspeed'],
+    x: dataset[0][0]['Timestamp'],
+    y: dataset[0][0]['Groundspeed'],
     text: labels,
     type: 'line',
     yaxis: 'y2',
@@ -70,149 +80,136 @@ var layout1 = {
 var data1 = [trace1,trace2];
 Plotly.newPlot("line",data1,layout1)
 
-/*
-for (var j = 0; j < dataset[].length; j++) {
+// Dynamically Create Options for Dropdown
+var dropdown = document.getElementById("selDataset");
+var opt = document.createElement("option"); 
+opt.text = 'Select Flight';
+opt.value = '';
+dropdown.options.add(opt);
+for (var j = 0; j < dataset[1][0]['Callsign'].length; j++) {
     var dropdown = document.getElementById("selDataset");
     var opt = document.createElement("option"); 
-    opt.text = subj[j].toString();
-    opt.value = subj[j].toString();
+    opt.text = dataset[1][0]['Callsign'][j].toString();
+    opt.value = dataset[1][0]['ICAO24'][j].toString();
     dropdown.options.add(opt);
 };
 
+var dropdown = document.getElementById("selDataset1");
+var opt = document.createElement("option"); 
+opt.text = 'Select Origin';
+opt.value = '';
+dropdown.options.add(opt);
+for (var j = 0; j < dataset[1][0]['Origin'].length; j++) {
+    var dropdown = document.getElementById("selDataset1");
+    var opt = document.createElement("option"); 
+    opt.text = dataset[1][0]['Origin'][j].toString();
+    opt.value = dataset[1][0]['Origin'][j].toString();
+    dropdown.options.add(opt);
+};
 
-d3.selectAll("#selDataset").on("change", change_flight);
+// Function to Dynamically Populate Table
+function populateTable(rows, cells, content) {
+    table = document.createElement('table');
+    header = document.createElement('thead');
+    head = document.createElement('tr');
+    head.appendChild(document.createElement('th'));
+    head.cells[0].appendChild(document.createTextNode('Callsign'));
+    head.appendChild(document.createElement('th'));
+    head.cells[1].appendChild(document.createTextNode('Origin'));
+    head.appendChild(document.createElement('th'));
+    head.cells[2].appendChild(document.createTextNode('Diverted'));
+    head.appendChild(document.createElement('th'));
+    head.cells[3].appendChild(document.createTextNode('Landing'));
+    head.appendChild(document.createElement('th'));
+    head.cells[4].appendChild(document.createTextNode('Typecode'));
+    head.appendChild(document.createElement('th'));
+    head.cells[5].appendChild(document.createTextNode('ICAO24'));
+    header.appendChild(head)
+    table.appendChild(header)
+    body = document.createElement('tbody');
 
-function change_flight() {
-    var dropdownMenu = d3.select("#selDataset");
-    var new_flight = dropdownMenu.property("value");
-    window.location.href = ('http://127.0.0.1:5000/flight_dashboard'+new_flight);
-}
-*/
-
-/*
-// Default Flight Trajectory Map
-var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-
-var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});
-
-// Importing Data
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-d3.json(url).then(function(response) {
-    console.log(response)
-
-    var quakeMarkers = [];
-
-    for (var i = 0; i < response.features.length; i++) {
-        var location = response.features[i];
-    
-        if (location) {
-
-            if (location.geometry.coordinates[2] < 10) {color = 'green'}
-            else if (location.geometry.coordinates[2] < 30) {color = 'greenyellow'}
-            else if (location.geometry.coordinates[2] < 50) {color = 'yellow'}
-            else if (location.geometry.coordinates[2] < 70) {color = 'orange'}
-            else if (location.geometry.coordinates[2] < 90) {color = 'orangered'}
-            else {color = 'red'}
-
-            quakeMarkers.push(
-                L.circle([location.geometry.coordinates[1], location.geometry.coordinates[0]],
-                    {
-                        color: 'black',
-                        weight: 1,
-                        fillColor: color,
-                        fillOpacity: 0.6,
-                        radius: Math.sqrt(Math.abs(location.properties.mag)) ** 2 * 10000
-                    })
-                    .bindPopup("<h3>Magnitude "+location.properties.mag+"</h3><hr>"
-                        +'<span style="font-weight: bold">Location:</span> ' + location.properties.place 
-                        + '<br><span style="font-weight: bold">Depth:</span> ' + location.geometry.coordinates[2]
-                        + '<br><span style="font-weight: bold">ID:</span> ' + location.id
-                        )
-            )
+    for (var i = 0; i < rows; ++i) {
+        row = document.createElement('tr');
+        for (var j = 0; j < cells; ++j) {
+            row.appendChild(document.createElement('td'));
+            row.cells[j].appendChild(document.createTextNode(content[j][i]));
         }
-      };
-    
-    var url2 = "tectonic.json";
+        table.appendChild(row);
+    }
+    table.appendChild(body)
+    return table
+}
+filter_data = dataset[1][0]
 
-    d3.json(url2).then(function(tec) {
-        console.log(tec)
+// Event Handling to Filter Data
+d3.selectAll("#selDataset1").on("change", create_table);
 
-        var tectonicMarkers = [];
+filter_data = dataset[1][0]
+cols = ['Callsign','Origin','Diverted','Landing','Typecode','ICAO24'];
 
-        for (var i=0; i < tec.features.length; i++) {
-            var location = tec.features[i];
-            for (var x=0; x < location.geometry.coordinates.length; x++) {
-                var tmp = location.geometry.coordinates[x][0];
-                location.geometry.coordinates[x][0] = location.geometry.coordinates[x][1]
-                location.geometry.coordinates[x][1] = tmp;
-            };
 
-            tectonicMarkers.push(
-                L.polyline([location.geometry.coordinates],
-                    {color: 'black',weight: 3})
-                    .bindPopup("<h3>Boundary Name: "+location.properties.Name+"</h3><hr>"
-                        +'<span style="font-weight: bold">Plate A:</span> ' + location.properties.PlateA 
-                        + '<br><span style="font-weight: bold">Plate B:</span> ' + location.properties.PlateB
-                        + '<br><span style="font-weight: bold">Source:</span> ' + location.properties.Source 
-                        )
-            )    
-        };
-
-    // Create overlay groups
-    var quakes = L.layerGroup(quakeMarkers);
-    var tectonic = L.layerGroup(tectonicMarkers);
-
-    // Create a baseMaps object.
-    var baseMaps = {
-        "Street Map": street,
-        "Topographic Map": topo
+function filter_func() {
+    for (let x=0; x<filter_data;x++) {
+        if (filter_data['Origin'][x] != d3.select("#selDataset").property("value")) {
+            for (var y in cols) {
+                filter_data[y].splice(x,1)
+            }
+        }
     };
-    
-    // Create an overlay object.
-    var overlayMaps = {
-        "Earthquakes": quakes,
-        "Tectonic Boundaries": tectonic
-    };
+    row_count = filter_data['Callsign'].length;
+    let filter_content = {};
 
-    // Define a map object.
-    let myMap = L.map("map", {
-        center: [37.09, -120],
-        zoom: 6,
-        layers: [street, quakes, tectonic]
-    });
-    
-    // Pass our map layers to our layer control.
-    // Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
+    for (var i=0; i<cols.length;i++) {
+        filter_content[i] = filter_data[cols[i]]
+    }
+    return filter_content
+};
 
-    // Add legend to map
-    var legend = L.control({position: 'bottomright'});
-    var colors = ['green','greenyellow','yellow','orange','orangered','red'];
+// Create the Table
+function create_table() {
+    tab = document.getElementById('flight_table');
+    tab.innerHTML='';
+    tab.appendChild(populateTable(filter_data['Callsign'].length, cols.length, filter_func()));
+};
+create_table()
 
-    legend.onAdd = function (myMap) {
-    
-        var div = L.DomUtil.create('div', 'info legend'),
-            grades = ['<10', '10-30', '30-50', '50-70', '70-90','>90'],
-            labels = [];
-        div.innerHTML += "<h4>Legend</h4>";
-        div.innerHTML += "<h5>(Epicenter Depth)</h5>";
-    
-        for (var i = 0; i < colors.length; i++) {
-            div.innerHTML +=
-                    '<i style="background:' + colors[i] + '"></i><span>' + grades[i] + '</span><br>'}
-        return div;
-    };
-    
-    legend.addTo(myMap);
+// Event Handling to highlight table rows and change flights on click
+d3.selectAll("tr")
+.on("mouseover", function(){
+    d3.select(this)
+        .style("background-color", "orange");
+})
+.on("mouseout", function(){
+    d3.select(this)
+        .style("background-color", "white")
+})
+.on("click", change_flight);
 
-    });
-});
 
-*/
+// Function to change Displayed Data with User Click
+function change_flight() {
+    var dropdownMenu = d3.selectAll('td')
+    output = d3.select(this).text()
+    new_flight = output.substring(output.length - 6)
+    window.location.href = ('http://127.0.0.1:5000/flight_dashboard_'+new_flight);
+}
+
+// Flight Trajectory Map
+var lats = dataset[0][0]['Latitude'];
+var lons = dataset[0][0]['Longitude'];
+let coords = [];
+for (let x=0;x<lats.length;x++) {
+    coords.push([lats[x],lons[x]])
+};
+middle_coords = coords[Math.round(lats.length / 2)];
+
+let myMap = L.map("map").setView(middle_coords, 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+L.polyline(coords,
+    {color: 'black',weight: 3})
+    .bindPopup("<h3>Flight Path: "+dataset[0][0]['Callsign'][0]+"</h3><hr>")
+    .addTo(myMap)
